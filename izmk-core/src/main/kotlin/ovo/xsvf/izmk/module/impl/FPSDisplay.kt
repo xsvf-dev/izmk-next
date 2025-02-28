9ca6b44f4f803dc2357c52da43576ce4c7e0a854
Chen Meng <miaoyuxuan10@outlook.com>
2025-02-28 16:56:56 +0800

    feat: Supported RenderableModule for FPS Display module

--- izmk-core/src/main/kotlin/ovo/xsvf/izmk/module/impl/FPSDisplay.kt
+++ izmk-core/src/main/kotlin/ovo/xsvf/izmk/module/impl/FPSDisplay.kt
@@ -5,11 +5,13 @@ import ovo.xsvf.izmk.event.EventTarget
 import ovo.xsvf.izmk.event.impl.Render2DEvent
 import ovo.xsvf.izmk.graphics.color.ColorRGB
 import ovo.xsvf.izmk.graphics.font.FontRenderers
-import ovo.xsvf.izmk.module.Module
+import ovo.xsvf.izmk.module.RenderableModule
 
-object FPSDisplay: Module(
+object FPSDisplay: RenderableModule(
     name = "fps-display",
     description = "Displays the FPS on the screen",
+    defaultX = 20f, defaultY = 20f,
+    width = 40f, height = 15f
 ) {
     private val drawString by setting("draw-string", true)
 
@@ -18,8 +20,7 @@ object FPSDisplay: Module(
         if (drawString) {
             FontRenderers.drawString(
                 "FPS: ${IZMK.mc.fps}",
-                20f, 20f,
-                ColorRGB.WHITE
+                x, y, ColorRGB.WHITE
             )
         }
     }
