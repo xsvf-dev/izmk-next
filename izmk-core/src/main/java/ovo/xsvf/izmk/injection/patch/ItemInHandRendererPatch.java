60a8344171e10c25e9c55304290f80f3691d509d
xsvf-dev <xsvf0721@163.com>
2025-03-09 17:08:30 +0800

    feat: old animations

--- izmk-core/src/main/java/ovo/xsvf/izmk/injection/patch/ItemInHandRendererPatch.java
+++ izmk-core/src/main/java/ovo/xsvf/izmk/injection/patch/ItemInHandRendererPatch.java
@@ -1,6 +1,7 @@
 package ovo.xsvf.izmk.injection.patch;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -8,64 +9,47 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.HumanoidArm;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
-import net.minecraft.world.item.UseAnim;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import ovo.xsvf.izmk.IZMK;
 import ovo.xsvf.izmk.module.impl.OldAnimations;
 import ovo.xsvf.patchify.CallbackInfo;
-import ovo.xsvf.patchify.annotation.*;
-import ovo.xsvf.patchify.api.Invocation;
+import ovo.xsvf.patchify.annotation.At;
+import ovo.xsvf.patchify.annotation.Inject;
+import ovo.xsvf.patchify.annotation.Patch;
+import ovo.xsvf.patchify.annotation.Slice;
 
 @Patch(ItemInHandRenderer.class)
 public class ItemInHandRendererPatch {
-    private static final Logger log = LogManager.getLogger(ItemInHandRendererPatch.class);
+    private static final Minecraft mc = Minecraft.getInstance();
 
-    @WrapInvoke(method = "renderArmWithItem", desc = "(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
-                target = "net/minecraft/world/item/ItemStack/getUseAnimation", targetDesc = "()Lnet/minecraft/world/item/UseAnim;")
-    public static UseAnim getUseAnimation(ItemInHandRenderer self, AbstractClientPlayer pPlayer,
-                                          float pPartialTicks, float pPitch, InteractionHand pHand,
-                                          float pSwingProgress, ItemStack pStack, float pEquippedProgress,
-                                          PoseStack pPoseStack, MultiBufferSource pBuffer, int pCombinedLight,
-                                          Invocation invocation) throws Exception {
-        if (OldAnimations.INSTANCE.getEnabled() && OldAnimations.INSTANCE.getSwordBlocking() &&
-                ((ItemStack) invocation.instance()).getItem() instanceof SwordItem) {
-           return UseAnim.BLOCK;
-        }
-        return (UseAnim) invocation.call();
-    }
-
-    @Inject(method = "renderArmWithItem", desc = "(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
-             slice = @Slice(startIndex = 5, endIndex = 5),
-             at = @At(value = At.Type.AFTER_INVOKE, method = "net/minecraft/client/renderer/ItemInHandRenderer/applyItemArmTransform", desc = "(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/HumanoidArm;F)V"))
-    public static void transformBlocking(ItemInHandRenderer self, AbstractClientPlayer pPlayer,
-                                         float pPartialTicks, float pPitch, InteractionHand pHand,
-                                         float pSwingProgress, ItemStack pStack, float pEquippedProgress,
-                                         PoseStack pPoseStack, MultiBufferSource pBuffer,
-                                         int pCombinedLight, CallbackInfo ci) {
-        if (IZMK.mc.options.keyUse.isDown() && OldAnimations.INSTANCE.getEnabled() && OldAnimations.INSTANCE.getSwordBlocking() &&
-                pStack.getItem() instanceof SwordItem) {
-            OldAnimations.INSTANCE.transform(
-                    pPoseStack,
-                    pHand == InteractionHand.MAIN_HAND ? HumanoidArm.RIGHT : HumanoidArm.LEFT,
-                    pEquippedProgress,
-                    pSwingProgress
-            );
+    @Inject(method = "renderArmWithItem",
+            desc = "(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+            at = @At(value = At.Type.AFTER_INVOKE, method = "net/minecraft/client/renderer/ItemInHandRenderer/applyItemArmTransform", desc = "(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/HumanoidArm;F)V"),
+            slice = @Slice(startIndex = 2, endIndex = 6))
+    public static void applySwingOffset(ItemInHandRenderer instance, AbstractClientPlayer pPlayer, float pPartialTicks, float pPitch, InteractionHand pHand, float pSwingProgress, ItemStack pStack, float pEquippedProgress, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pCombinedLight, CallbackInfo callbackInfo) {
+        if (OldAnimations.INSTANCE.getSwing()) {
+            OldAnimations.INSTANCE.applySwingOffset(pMatrixStack,  pHand == InteractionHand.MAIN_HAND ? HumanoidArm.RIGHT : HumanoidArm.LEFT, pSwingProgress);
         }
     }
 
-    @WrapInvoke(method = "renderArmWithItem", desc = "(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
-            slice = @Slice(startIndex = 5, endIndex = 5),
-            target = "net/minecraft/client/renderer/ItemInHandRenderer/applyItemArmTransform", targetDesc = "(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/HumanoidArm;F)V")
-    public static void translate(ItemInHandRenderer self, AbstractClientPlayer pPlayer,
-                                 float pPartialTicks, float pPitch, InteractionHand pHand,
-                                 float pSwingProgress, ItemStack pStack, float pEquippedProgress,
-                                 PoseStack pPoseStack, MultiBufferSource pBuffer,
-                                 int pCombinedLight, Invocation invocation) throws Exception {
-        if (IZMK.mc.player != null && IZMK.mc.options.keyUse.isDown() && OldAnimations.INSTANCE.getEnabled() &&
-                OldAnimations.INSTANCE.getSwordBlocking() && pStack.getItem() instanceof SwordItem) {
-            invocation.args().set(3, -0.52f);
+    @Inject(method = "renderArmWithItem",
+            desc = "(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+            at = @At(value = At.Type.AFTER_INVOKE, method = "net/minecraft/client/renderer/ItemInHandRenderer/applyItemArmTransform", desc = "(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/HumanoidArm;F)V"),
+            slice = @Slice(startIndex = 5, endIndex = 5))
+    public static void onBlocking0(ItemInHandRenderer instance, AbstractClientPlayer pPlayer, float pPartialTicks, float pPitch, InteractionHand pHand, float pSwingProgress, ItemStack pStack, float pEquippedProgress, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pCombinedLight, CallbackInfo callbackInfo) {
+        onBlocking(pHand, pMatrixStack, pStack);
+    }
+
+    @Inject(method = "renderArmWithItem",
+            desc = "(Lnet/minecraft/client/player/AbstractClientPlayer;FFLnet/minecraft/world/InteractionHand;FLnet/minecraft/world/item/ItemStack;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;I)V",
+            at = @At(value = At.Type.AFTER_INVOKE, method = "net/minecraft/client/renderer/ItemInHandRenderer/applyItemArmTransform", desc = "(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/world/entity/HumanoidArm;F)V"),
+            slice = @Slice(startIndex = 9, endIndex = 9))
+    public static void onBlocking1(ItemInHandRenderer instance, AbstractClientPlayer pPlayer, float pPartialTicks, float pPitch, InteractionHand pHand, float pSwingProgress, ItemStack pStack, float pEquippedProgress, PoseStack pMatrixStack, MultiBufferSource pBuffer, int pCombinedLight, CallbackInfo callbackInfo) {
+        onBlocking(pHand, pMatrixStack, pStack);
+    }
+
+    private static void onBlocking(InteractionHand hand, PoseStack poseStack, ItemStack itemStack) {
+        if (OldAnimations.INSTANCE.getSwordBlocking() && itemStack.getItem() instanceof SwordItem &&
+                mc.options.keyUse.isDown()) {
+            OldAnimations.INSTANCE.transformBlockFirstPerson(poseStack, hand == InteractionHand.MAIN_HAND ? HumanoidArm.RIGHT : HumanoidArm.LEFT);
         }
-        invocation.call();
     }
 }
--- izmk-core/src/main/kotlin/ovo/xsvf/izmk/module/impl/OldAnimations.kt
+++ izmk-core/src/main/kotlin/ovo/xsvf/izmk/module/impl/OldAnimations.kt
@@ -3,27 +3,46 @@ package ovo.xsvf.izmk.module.impl
 import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.math.Axis
 import net.minecraft.util.Mth
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.effect.MobEffectUtil
+import net.minecraft.world.effect.MobEffects
 import net.minecraft.world.entity.HumanoidArm
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.phys.HitResult
+import ovo.xsvf.izmk.event.EventTarget
+import ovo.xsvf.izmk.event.impl.PreTickEvent
 import ovo.xsvf.izmk.module.Module
 
 object OldAnimations: Module(name = "old-animations") {
-    val swordBlocking by setting("sword-blocking", true)
+    val swordBlocking by setting("sword-blocking", false)
+    val swing by setting("swing", false)
 
-    fun transform(matrices: PoseStack, arm: HumanoidArm, equipProgress: Float, swingProgress: Float) {
-        matrices.translate(if (arm == HumanoidArm.RIGHT) -0.1f else 0.1f, 0.1f, 0.0f)
-        applySwingOffset(matrices, arm, swingProgress * 0.9f)
-        matrices.mulPose(Axis.XP.rotationDegrees(-102.25f))
-        matrices.mulPose(
-            (if (arm == HumanoidArm.RIGHT) Axis.YP else Axis.YN)
-                .rotationDegrees(13.365f)
-        )
-        matrices.mulPose(
-            (if (arm == HumanoidArm.RIGHT) Axis.ZP else Axis.ZN)
-                .rotationDegrees(78.05f)
-        )
+    @EventTarget
+    fun onTick(event: PreTickEvent) {
+        mc.player ?: return
+        val hand = mc.player!!.usedItemHand
+        if (!swing || mc.player!!.getItemInHand(hand).isEmpty ||
+            !mc.options.keyAttack.isDown || !mc.player!!.isUsingItem) {
+            return
+        }
+
+        if (mc.hitResult != null && mc.hitResult!!.type === HitResult.Type.BLOCK &&
+            hand == InteractionHand.MAIN_HAND) {
+            fakeHandSwing(mc.player!!, hand)
+        }
     }
 
-    private fun applySwingOffset(matrices: PoseStack, arm: HumanoidArm, swingProgress: Float) {
+    fun transformBlockFirstPerson(matrixStack: PoseStack, hand: HumanoidArm) {
+        val direction = if (hand == HumanoidArm.RIGHT) 1 else -1
+        // values taken from Minecraft snapshot 15w33b
+        matrixStack.translate(direction * -0.14142136f, 0.08f, 0.14142136f)
+        matrixStack.mulPose(Axis.XP.rotationDegrees(-102.25f))
+        matrixStack.mulPose(Axis.YP.rotationDegrees(direction * 13.365f))
+        matrixStack.mulPose(Axis.ZP.rotationDegrees(direction * 78.05f))
+    }
+
+    fun applySwingOffset(matrices: PoseStack, arm: HumanoidArm, swingProgress: Float) {
         val armSide = if (arm == HumanoidArm.RIGHT) 1 else -1
         val f = Mth.sin(swingProgress * swingProgress * Math.PI.toFloat())
         matrices.mulPose(Axis.YP.rotationDegrees(armSide.toFloat() * (45.0f + f * -20.0f)))
@@ -31,5 +50,24 @@ object OldAnimations: Module(name = "old-animations") {
         matrices.mulPose(Axis.ZP.rotationDegrees(armSide.toFloat() * g * -20.0f))
         matrices.mulPose(Axis.XP.rotationDegrees(g * -80.0f))
         matrices.mulPose(Axis.YP.rotationDegrees(armSide.toFloat() * -45.0f))
+    }
+
+    private fun fakeHandSwing(player: Player, hand: InteractionHand) {
+        if (!player.swinging || player.swingTime >= getHandSwingDuration(player) / 2 || player.swingTime < 0) {
+            player.swingTime = -1
+            player.swinging = true
+            player.swingingArm = hand
+        }
+    }
+
+    private fun getHandSwingDuration(entity: LivingEntity): Int {
+        if (MobEffectUtil.hasDigSpeed(entity)) {
+            return 6 - (1 + MobEffectUtil.getDigSpeedAmplification(entity))
+        } else if (entity.hasEffect(MobEffects.DIG_SLOWDOWN)) {
+            val effect = entity.getEffect(MobEffects.DIG_SLOWDOWN)
+            return if (effect == null) 6 else 6 + (1 + effect.amplifier) * 2
+        } else {
+            return 6
+        }
     }
 }
